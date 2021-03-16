4ed10ba61d39c1ebb6bf5a9be5b244ddc39f2213
jie <xiguiwsj@163.com>
2021-03-16 18:20:22 +0800

    i

---
+++ wsj-gateway/src/main/java/com/wsj/configure/GatewaySecurityConfigure.java
@@ -0,0 +1,19 @@
+package com.wsj.configure;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Auther: wsj
+ * @Date: 2021/3/16 16
+ * @Description:
+ */
+@EnableWebSecurity
+public class GatewaySecurityConfigure extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+    }
+}
